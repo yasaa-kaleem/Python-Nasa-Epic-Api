ca15d541c0bfd974a55e249d44e214553d07872a
Yasaa Kaleem <yasaa.kaleem.ee1h@statefarm.com>
2024-07-25 09:05:21 -0500

    Adding changes to update image data function to get data from epic api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import nasa_api, webbrowser
 
-epic_image = nasa_api.get_nasa_epic_image_data("2024", "07", "21", "png")
+epic_image = nasa_api.get_nasa_epic_image_data()
 print("Opening Image...")
 webbrowser.open(epic_image)
 print("Image opened! Exiting program...")
